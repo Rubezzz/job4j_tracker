e465d3a41d64f9762d40b79f441717f579865968
Rubezzz <inigor1@mail.ru>
2023-01-21 23:31:12 +0700

    9. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,22 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    private int sizeOfEmpty = 10;
+    private String surname;
+    String name;
+    public static final String NEW_VALUE = "";
+
+    public Broken() { }
+
+    public void echo() { }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+
+    }
+
+    public void method(int a, int b, int c, int d, int e) { }
+}
